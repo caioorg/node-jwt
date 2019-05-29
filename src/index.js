11404e611e0e57c0191295c09de8ec66571a62e3
caioalmeidasilva96@gmail.com <caioalmeidasilva96@gmail.com>
2019-05-28 23:47:04 -0300

    enable cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const port = 3003
 const express = require('express')
 const bodyParser = require('body-parser')
+const allowCors = require('./config/cors')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(allowCors)
 
 require('./app/controllers/index')(app)
 
